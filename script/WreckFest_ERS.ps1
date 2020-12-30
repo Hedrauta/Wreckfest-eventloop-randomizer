c30db3f13885a956e154969774c106c8fec4ebdf
H3draut3r <H3draut3r@hotmail.de>
2020-12-30 21:12:29 +0100

    Update WreckFest_ERS.ps1
    lesser code... this will get interesting :D

--- script/WreckFest_ERS.ps1
+++ script/WreckFest_ERS.ps1
@@ -11,18 +11,17 @@ sleep 1
 "" ; "" ; ""
 Write-Host "Preparing types"
 ""
-[System.Collections.ArrayList]$types = $(Get-ChildItem -Exclude WreckFest_ERS.ps1,eventloop.txt).BaseName #fetching .txt-files, and define them without extension in an array
-[System.Collections.ArrayList]$types_file = $(Get-ChildItem -Exclude WreckFest_ERS.ps1,eventloop.txt) #fetching .txt-files, and define them in an array
+[System.Collections.ArrayList]$types = $(Get-ChildItem -Exclude WreckFest_ERS.ps1,eventloop.txt) #fetching .txt-files, and define them in an array
 
 for ($i=0; $i -le $($types.Count - 1) ; $i++) {
-    Write-Host "$($types[$i]).txt found" # oh boy, this one is working fine.... i will get this possible
+    Write-Host "$($($types[$i]).Name) found"
         $rh_i = $null
     do {
         $rh_i = $null
-        $rh_i = Read-Host -Prompt "Do you want load $($types[$i]).txt?"
+        $rh_i = Read-Host -Prompt "Do you want load $($($types[$i]).Name)?"
         if ($rh_i -eq 'y') {
-            Set-Variable $($types[$i]) -Value $(Get-Content $($types_file[$i])) #define the map-variable and load them with the maps-content!!
-            Write-Host "File with $($($(Get-Variable $types[$i]).Value).Count) Maps loaded" # .this).took).me).a).while).to).get).it)
+            Set-Variable $($($types[$i]).BaseName) -Value $(Get-Content $($($types[$i]).Name)) #define the map-variable and load them with the maps-content!!
+            Write-Host "File with $($($(Get-Variable $($($types[$i]).BaseName)).Value).Count) Maps loaded" # .this).took).me).a).while)).to).get).it)
             }
         } until ($rh_i -eq 'y' -or $rh_i -eq 'n')
     }
